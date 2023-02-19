1e626d192eda8b3f17e8c3f0d0d65a3494a72673
FredericWantiez <frederic.wantiez@gmail.com>
2023-02-19 10:07:56 +0000

    Bakcup

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,6 +3,14 @@ require("highlights")
 require("keymap")
 require("settings")
 
+local autocmd = vim.api.nvim_create_autocmd
+
 vim.g.netrw_browse_split = 0
 vim.wo.number = true
 vim.wo.cursorline = true
+
+autocmd({"BufWritePre"}, {
+    group = ThePrimeagenGroup,
+    pattern = "*",
+    command = [[%s/\s\+$//e]],
+})
